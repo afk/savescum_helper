020ffc7941c9917434debd6a5231ef55d70f3e53
Efe Aydın <3feaydin@gmail.com>
2019-01-12 22:46:18 +0300

    description changed

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # savescum_helper
-A tool to help you savescum by backing up saves when they are modified
+A tool to back up saves when they are modified
 
 ## Installation
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="savescum_helper",
     version="0.1.0",
-    description="A simple tool to help people savescum",
+    description="A tool to back up saves when they are modified",
     url="https://github.com/afk/savescum_helper",
     author="Efe Aydın",
     py_modules=["savescum_helper"],
