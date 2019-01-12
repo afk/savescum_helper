8d9aa64abae585b0208f26399cbdd3f7afb7e0b5
Efe Aydın <3feaydin@gmail.com>
2019-01-12 22:11:56 +0300

    format setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='savescum_helper',
-    version='0.1.0',
-    description='A simple tool to help people savescum',
-    url='https://github.com/afk/savescum_helper',
-    author='Efe Aydın',
-    py_modules=['savescum_helper'],
-    entry_points={
-        'console_scripts': ['savescum_helper=savescum_helper:main']
-    }
+    name="savescum_helper",
+    version="0.1.0",
+    description="A simple tool to help people savescum",
+    url="https://github.com/afk/savescum_helper",
+    author="Efe Aydın",
+    py_modules=["savescum_helper"],
+    entry_points={"console_scripts": ["savescum_helper=savescum_helper:main"]},
 )
